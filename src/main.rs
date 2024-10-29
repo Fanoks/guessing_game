df682d290d649eb684cb7c4a3ba543eeafc69e3d
Jakub Konieczny <126960714+Fanoks@users.noreply.github.com>
2024-10-29 22:38:08 +0100

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use rand::Rng;
+
+
+fn main() -> () {
+    let stdin: io::Stdin = io::stdin();
+    let mut input: String = String::new();
+    
+    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Give your guess: ");
+        input.clear();
+        stdin.read_line(&mut input).unwrap();
+
+        match input.trim().parse::<u8>() {
+            Ok(guess) => {
+                if guess < secret_number {
+                    println!("Too small!");
+                } else if guess > secret_number {
+                    println!("Too big!");
+                } else {
+                    println!("You won!");
+                    break;
+                }
+            }
+            Err(_) => {
+                println!("Please enter a valid number!");
+            }
+        }
+    }
 }
